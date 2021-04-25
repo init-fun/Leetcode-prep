8c7ef216d40635adbf5c9c24f5abc2b342413854
init_fun <sde.kapilraj@gmail.com>
2021-04-25 11:25:29 +0530

    Time: 40 ms (94.53%), Space: 14.4 MB (72.54%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        fptr = 0
-        while fptr < len(nums):
-            sptr = fptr+1
-            while sptr < len(nums):
-                if nums[fptr] + nums[sptr] == target:
-                    return [fptr, sptr]
-                sptr += 1
-            fptr += 1
-        else:
-            return False
+        tmp = {}
+        for index, val in enumerate(nums):
+            # val + y = target
+            # y = target - val
+            y = target - val
             
+            if y in tmp:
+                second_index = tmp[y]
+                return [index, second_index]
+            else:
+                tmp[val] = index
