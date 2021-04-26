569f1a51567783514f976f99de462ea1dd828cdb
init_fun <sde.kapilraj@gmail.com>
2021-04-26 22:52:24 +0530

    Time: 32 ms (85.13%), Space: 15.6 MB (43.85%) - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+
+        prev_node = None
+        cnode = head
+        while cnode:
+            next_node = cnode.next
+            cnode.next = prev_node
+            prev_node = cnode
+            cnode = next_node
+        return prev_node
