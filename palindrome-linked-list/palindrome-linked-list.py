1da5ed95383548d034e295078bac8334cee9a64d
init_fun <sde.kapilraj@gmail.com>
2021-06-20 23:40:52 +0530

    Time: 752 ms (87.65%), Space: 39.3 MB (79.20%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.py
+++ palindrome-linked-list/palindrome-linked-list.py
@@ -5,32 +5,41 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        slow = head
+        # first go to the middle point of the lst
+        # and then reverse the list and checkout whether they are equal or not
+        
         fast = head
-        
+        slow = head
         while fast and fast.next:
-            fast = fast.next.next
             slow = slow.next
+            fast = fast.next.next
         
-        # we got our middle at slow
-        # reversing the list from the middle
-        prev = None
+        # slow is the first node of the second half
+        
+        prev_node = None
         while slow:
             next_node = slow.next
-            slow.next = prev
-            prev = slow
+            slow.next = prev_node
+            prev_node = slow
             slow = next_node
         
-        # prev node is at the start of the reverse list
-        # now just traverse both the start 
-        
-        left = head 
-        right = prev
-        while right:
-            if left.val != right.val:
+        # prev_node is the first node of the second half
+        while prev_node:
+            if prev_node.val != head.val:
                 return False
-            left = left.next
-            right = right.next
+            prev_node = prev_node.next
+            head = head.next
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
