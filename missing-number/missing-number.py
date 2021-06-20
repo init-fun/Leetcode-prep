300dcc15621c1701f4da0f4773ca195d947a44d0
init_fun <sde.kapilraj@gmail.com>
2021-06-20 16:20:53 +0530

    Time: 116 ms (98.80%), Space: 15.5 MB (16.15%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums) + 1):
-            if i not in nums:
-                return i
-            
+        n = len(nums) + 1
+        actual_sum = (n * (n-1))//2
+        arr_sum = sum(nums)
+        return abs(actual_sum - arr_sum)
