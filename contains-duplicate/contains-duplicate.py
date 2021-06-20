ec4e7a07c1005f3154dd39930e165dc9bfc07f53
init_fun <sde.kapilraj@gmail.com>
2021-06-20 16:08:15 +0530

    Time: 112 ms (92.00%), Space: 20.2 MB (80.96%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,6 +1,4 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        x = collections.Counter(nums)
-        for i,val in x.items():
-            if val > 1:
-                return True
+        return len(set(nums)) != len(nums)
+        
