ae4236c3b211585beb118718eb9620aad61b1f3a
init_fun <sde.kapilraj@gmail.com>
2021-06-20 23:56:38 +0530

    Time: 72 ms (50.64%), Space: 17.2 MB (23.40%) - LeetHub

--- remove-linked-list-elements/remove-linked-list-elements.py
+++ remove-linked-list-elements/remove-linked-list-elements.py
@@ -4,16 +4,18 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeElements(self, head: ListNode, data: int) -> ListNode:
-        dummy_head = ListNode(-1)
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy_head = ListNode(0)
         dummy_head.next = head
         
-        cnode= dummy_head
-        while cnode.next: # this is the first element of our linkedlist
-            if cnode.next.val == data:
+        cnode = dummy_head
+        
+        while cnode.next:
+            if cnode.next.val == val:
                 cnode.next = cnode.next.next
             else:
-                cnode =cnode.next
+                cnode = cnode.next
+            
         return dummy_head.next
-                
-        
+            
+            
