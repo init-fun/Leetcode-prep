10191e822cc890bcd6504ddf3d107a50431a97ad
init_fun <sde.kapilraj@gmail.com>
2021-06-20 17:21:27 +0530

    Time: 136 ms (54.31%), Space: 16.7 MB (58.27%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,3 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-         return 2* sum(set(nums)) - sum(nums)
+        
+        # space = O(N)
+        #time = O(N)
+        d = dict()
+        for i in nums:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        for i,v in d.items():
+            if v < 2:
+                return i
+        
