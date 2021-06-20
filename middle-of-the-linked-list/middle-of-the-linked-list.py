d1c671f5554775e870407ddac43ed8bf2ba75a2f
init_fun <sde.kapilraj@gmail.com>
2021-06-20 21:01:14 +0530

    Time: 24 ms (95.08%), Space: 14.2 MB (70.67%) - LeetHub

--- middle-of-the-linked-list/middle-of-the-linked-list.py
+++ middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -3,22 +3,15 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from math import ceil
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
-        count = 0
-        cnode = head
-        while cnode: # O(n)
-            cnode = cnode.next
-            count += 1
-        # print(count)
-        mid_count = count//2
-        count = 0
-        cnode = head
-        while count < mid_count:
-            count += 1
-            cnode = cnode.next
-            
+        fast = head
+        slow = head
         
-        return cnode
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow
+            
         
