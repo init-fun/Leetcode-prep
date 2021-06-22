fb2b973dbce5d2254a3be23d68a6153a1579e0ca
init_fun <sde.kapilraj@gmail.com>
2021-06-22 16:53:19 +0530

    Time: 60 ms (52.03%), Space: 15.4 MB (12.89%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # x + y = target
-        # target - curr = y 
-        # if y in list then return the num else not present
-        tmp = {}
-        for index, val in enumerate(nums):
-            y = target - nums[index]
-            if y in tmp:
-                return [index, tmp[y]]
+        d = dict()
+        for i,v in enumerate(nums):
+            tmp_sum = target - v
+            if tmp_sum not in d:
+                d[v] = i
             else:
-                tmp[val] = index 
+                return [i,d[tmp_sum]]
                 
-                
+        
